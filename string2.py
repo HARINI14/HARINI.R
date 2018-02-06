b655df10337ad20c5587eacf831403a79217f649
HARINI14 <35484518+HARINI14@users.noreply.github.com>
2018-02-06 15:32:30 +0530

    Add files via upload

---
+++ string2.py
@@ -0,0 +1,15 @@
+#string program
+my_string ='''HARINI'''
+print (my_string[0])
+print(my_string[0:6])
+print(my_string[0:5])
+print(my_string[0:4])
+print(my_string[0:3])
+print(my_string[0:2])
+print(my_string[0:1])
+print(my_string[1])
+print(my_string[2])
+print(my_string[3])
+print(my_string[4])
+print(my_string[5])
+
